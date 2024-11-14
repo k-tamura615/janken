d9d18b37723578fa16153db07c5e78be9aaa3613
Koshi Tamura <k.tamura.sys24@morijyobi.ac.jp>
2024-11-14 09:45:58 +0900

    Delete player.py

--- player.py
+++
@@ -1,7 +0,0 @@
-def pon():
-    while True:
-        user_input = int(input("あなたの手を選択してください。\n1.グー 2.チョキ 3.パー\n> "))
-        if user_input in [1, 2, 3]:
-            return user_input
-        else:
-            print("1, 2, 3のいずれかの数字を入力してください。")
