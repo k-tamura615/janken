b2648a93fd47bd6ae892e5cbde23f215e16c197f
Koshi Tamura <k.tamura.sys24@morijyobi.ac.jp>
2024-11-14 09:46:36 +0900

    Delete janken_jadge.py

--- janken_jadge.py
+++
@@ -1,7 +0,0 @@
-def judge(player_hand, computer_hand):
-    if player_hand == computer_hand:
-        return "あいこ"
-    elif (player_hand == 1 and computer_hand == 2) or (player_hand == 2 and computer_hand == 3) or (player_hand == 3 and computer_hand == 1):
-        return "勝ち"
-    else:
-        return "負け"
